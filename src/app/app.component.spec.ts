721841b042edd9bb90bfd5bb719d9e6b2db8dac2
kevvj <kevinjvillalba774+1@gmail.com>
2024-07-23 20:32:47 -0500

    letmealone

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'temp-project' title`, () => {
+  it(`should have the 'WashCar' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('temp-project');
+    expect(app.title).toEqual('WashCar');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, temp-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, WashCar');
   });
 });
